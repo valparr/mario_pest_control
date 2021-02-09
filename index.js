07b080dc465892b4859f2f888f48e565453c7d00
csherms <casey.sherman@bryanuniversity.edu>
2021-02-09 11:54:25 -0700

    second commit

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var totalButton = document.getElementById("totalButton");
 
 
 
+
 function getPrice() {
 var goomPrice = document.getElementById("goom").value;
 var bombPrice = document.getElementById("bomb").value;
 var cheepPrice = document.getElementById("cheep").value;
+var priceOutput = document.getElementById("out");
+
 
 var result = parseInt(goomPrice * 5) + parseInt(bombPrice * 7) + parseInt(cheepPrice * 11);
 
-alert(`Your total price for pest removal comes to... \n${result} coins.\nThank you for using Mario Pest Control!`);
+
+priceOutput.innerHTML ="Your total comes to: " + result + " Coins";
 }
+getPrice();
